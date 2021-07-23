13343819ea5f993712c75f36bae47947c32ce612
Emerson Fernandes <emerson_feral@hotmail.com>
2021-07-23 13:46:08 +0200

    Adding dotenv and mongoose connect

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const debug = require('debug');
 
+const { connect } = 'mongoose';
+
 const server = express();
 const port = 2030;
 
 server.use(express.json());
+
+connect(
+  process.env.DDBB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
 
 server.listen(port, () => debug(`Server is running in localhost:${port}`));
