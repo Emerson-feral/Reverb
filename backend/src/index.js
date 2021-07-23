ac520fc889e45405530f85401a76840be0823d85
Emerson Fernandes <emerson_feral@hotmail.com>
2021-07-23 16:01:30 +0200

    Dotenv call

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const debug = require('debug');
+const debug = require('debug')('server');
+require('dotenv').config();
 
-const { connect } = 'mongoose';
+const mongoose = require('mongoose');
 
 const server = express();
 const port = process.env.PORT;
 
 server.use(express.json());
 
-connect(
+mongoose.connect(
   process.env.DDBB_URL,
   {
     useUnifiedTopology: true,
     useNewUrlParser: true
   }
+).then(
+  () => debug('database connection stablished'),
+  (error) => debug('database connection error', error)
 );
 
 server.listen(port, () => debug(`Server is running in localhost:${port}`));
