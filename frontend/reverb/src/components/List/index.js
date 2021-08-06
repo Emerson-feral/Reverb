bcb45b93357ed2bee802f6a7260543615a0404c1
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-06 19:17:05 +0200

    Connect list component to the store

--- frontend/reverb/src/components/List/index.js
+++ frontend/reverb/src/components/List/index.js
@@ -1,13 +1,30 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
-import { View, Button } from 'react-native';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  View, Text, ScrollView
+} from 'react-native';
+import getGuitars from '../../redux/action/actionCreators';
 
-function List({ navigation }) {
+function List() {
+  const dispatch = useDispatch();
+  const guitars = useSelector((store) => store.guitars);
+
+  useEffect(() => {
+    if (!guitars.length)dispatch(getGuitars());
+  }, []);
+
   return (
     <View>
-      <Button onPress={() => navigation.navigate('Header')} title="Home">
-        Home
-      </Button>
+      <ScrollView>
+        {
+          guitars.map((item) => (
+            <View key={item._id} >
+              <Text>{item.brand}</Text> 
+            </View>
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
