a0b7efcfc3f04fb4e61f611d6d96e64277c344a3
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-06 19:28:34 +0200

    Adding cors and chalk to the server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
 const debug = require('debug')('server');
 const morgan = require('morgan');
-require('dotenv').config();
-
 const mongoose = require('mongoose');
-const guitarRoutes = require('./routes/guitarRoutes');
+require('dotenv').config();
 
 const server = express();
 const port = process.env.PORT;
@@ -17,9 +17,12 @@ mongoose.connect(
   }
 );
 
+server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
 
+const guitarRoutes = require('./routes/guitarRoutes');
+
 server.use('/api/guitar', guitarRoutes);
 
-server.listen(port, () => debug(`Server is running in localhost:${port}`));
+server.listen(port, () => debug(`Server is running in ${chalk.blue(`localhost:${port}`)}`));
