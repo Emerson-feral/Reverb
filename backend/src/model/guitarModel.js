e8b7666d09fe3147ae06c1da3167b997725116f6
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-10 19:15:05 +0200

    Adding more properties to guitarSchema

--- backend/src/model/guitarModel.js
+++ backend/src/model/guitarModel.js
@@ -4,8 +4,11 @@ const guitarSchema = mongoose.Schema({
   image: String,
   brand: String,
   model: String,
-  strings: Number,
-  price: Number
+  shape: String,
+  specs: {
+    colour: String, body: String, top: String, neck: String, fretboard: String, pickups: String
+  },
+  strings: Number
 });
 
 module.exports = mongoose.model('Guitar', guitarSchema);
