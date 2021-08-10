61236f592ece3318c2e59cd78e7e0906b9c08626
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-10 19:18:26 +0200

    Adding styles to the Home component

--- frontend/reverb/src/components/Home/index.js
+++ frontend/reverb/src/components/Home/index.js
@@ -1,17 +1,49 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import {
-  View, Text, TextInput
+  View, Text, ImageBackground, StyleSheet
 } from 'react-native';
+import BrandsList from '../BrandsList';
+
+const styles = StyleSheet.create({
+  appLogo: {
+    flex: 1,
+    position: 'absolute',
+    alignItems: 'center',
+    height: 520,
+    width: '100%'
+  },
+
+  appLogoText: {
+    fontSize: 40,
+    color: 'white',
+    marginTop: 30
+  },
+
+  mainContainer: {
+    height: '100%'
+  },
+
+  brandContainer: {
+    marginTop: '60%'
+  }
+});
 
 function Home() {
+  const image = { uri: 'https://freexwallpaper.com.hotbv.com/2020/11/17/2020_electric_guitars_guitars_musical_instruments_192754_2160x3840.jpg-postimg' };
+
   return (
-    <View>
-      <Text>Guitar Data Base</Text>
-      <TextInput placeholder="Search" />
-      {/* <Button onPress={() => navigation.navigate('List')} title="List">
-        List
-      </Button> */}
+    <View style={styles.mainContainer}>
+      <ImageBackground
+        style={styles.appLogo}
+        source={image}
+        resizeMode="stretch"
+      >
+        <Text style={styles.appLogoText}>Guitar Data Base</Text>
+      </ImageBackground>
+      <View style={styles.brandContainer}>
+        <BrandsList />
+      </View>
     </View>
   );
 }
