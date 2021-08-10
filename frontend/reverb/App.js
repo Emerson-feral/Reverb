bd9a8089c5c464ef741693d6d49961c29cb0fb86
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-10 19:16:04 +0200

    Changing the tab navigation for stack navigation

--- frontend/reverb/App.js
+++ frontend/reverb/App.js
@@ -10,31 +10,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Provider } from 'react-redux';
 
-/* import Navigation from './src/components/Navigation/index'; */
-import Home from './src/components/Home';
-import List from './src/components/List';
+import Navigation from './src/components/Navigation';
 import configureStore from './src/redux/store';
-
-const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <>
       <Provider store={configureStore()}>
         <NavigationContainer>
-          <Tab.Navigator initialRouteName="Home">
-            <Tab.Screen
-              name="Home"
-              component={Home}
-            />
-            <Tab.Screen
-              name="List"
-              component={List}
-            />
-          </Tab.Navigator>
+          <Navigation />
         </NavigationContainer>
       </Provider>
     </>
