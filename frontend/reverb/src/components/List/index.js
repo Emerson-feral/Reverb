5c8284439305785cb01c77d6345e180b8ae9c2e5
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-10 19:19:44 +0200

    Adding styles and routing the List component

--- frontend/reverb/src/components/List/index.js
+++ frontend/reverb/src/components/List/index.js
@@ -1,30 +1,79 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  View, Text, ScrollView
+  View, Text, ScrollView, Image, StyleSheet, ImageBackground
 } from 'react-native';
 import getGuitars from '../../redux/action/actionCreators';
 
-function List() {
+function List({ route }) {
   const dispatch = useDispatch();
   const guitars = useSelector((store) => store.guitars);
+  const { selectedBrand, logo } = route.params;
+  const brandLogo = { uri: logo };
 
   useEffect(() => {
     if (!guitars.length)dispatch(getGuitars());
   }, []);
 
+  const filteredGuitars = guitars?.filter((item) => item.brand === selectedBrand);
+
+  const styles = StyleSheet.create({
+    tinyLogo: {
+      width: 50,
+      height: 50,
+      margin: 30
+    },
+    appLogo: {
+      position: 'absolute',
+      height: '60%',
+      width: '100%'
+    },
+
+    listContainer: {
+      marginTop: '60%',
+      backgroundColor: 'blue',
+      borderTopLeftRadius: 60,
+      borderTopRightRadius: 60,
+      height: '100%'
+    },
+
+    guitarList: {
+      marginTop: 20
+    },
+    // eslint-disable-next-line react-native/no-color-literals
+    background: {
+      height: '100%'
+    }
+  });
+
   return (
-    <View>
-      <ScrollView>
-        {
-          guitars.map((item) => (
-            <View key={item._id} >
-              <Text>{item.brand}</Text> 
+    <View style={styles.background}>
+      <ImageBackground
+        style={styles.appLogo}
+        source={brandLogo}
+        resizeMode="stretch"
+      />
+      <View>
+        <ScrollView style={styles.listContainer}>
+          {
+          filteredGuitars?.map((item) => (
+            <View key={item._id} style={styles.guitarList}>
+              <Image
+                style={styles.tinyLogo}
+                source={{
+                  uri: item.image
+                }}
+              />
+              <Text>{item.model}</Text>
             </View>
           ))
         }
-      </ScrollView>
+        </ScrollView>
+
+      </View>
+
     </View>
   );
 }
