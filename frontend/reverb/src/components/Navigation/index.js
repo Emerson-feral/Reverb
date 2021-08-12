2eb83dba18b82f8c2b800a7282090346c6ee6324
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-12 19:29:38 +0200

    Adding GuitarDetail to the navigation

--- frontend/reverb/src/components/Navigation/index.js
+++ frontend/reverb/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import List from '../List';
 import Home from '../Home/index';
 import BrandsList from '../BrandsList';
+import GuitarDetail from '../GuitarDetail';
 
 const Stack = createStackNavigator();
 
@@ -16,8 +17,9 @@ export default function Navigation() {
       }}
     >
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="List" component={List} />
       <Stack.Screen name="BrandsList" component={BrandsList} />
+      <Stack.Screen name="List" component={List} />
+      <Stack.Screen name="GuitarDetail" component={GuitarDetail} />
     </Stack.Navigator>
 
   );
