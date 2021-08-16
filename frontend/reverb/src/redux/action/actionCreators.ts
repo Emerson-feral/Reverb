e01d375ff8df45d0a7f1127e78946eaca2d1791e
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-16 19:35:04 +0200

    Typing action creators

---
+++ frontend/reverb/src/redux/action/actionCreators.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from 'redux';
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const URL_API = 'http://10.0.2.2:2030/api/guitar';
+
+function getGuitars() {
+  return async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.get(URL_API);
+      dispatch({
+        type: actionTypes.LOAD_GUITARS,
+        guitars: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.LOAD_GUITARS_ERROR
+      });
+    }
+  };
+}
+
+export default getGuitars;
