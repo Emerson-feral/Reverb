828e662fabbfeef75f6cd28170b327ba6e9c7287
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-16 18:14:29 +0200

    New rules added to the eslint

--- frontend/reverb/.eslintrc.js
+++ frontend/reverb/.eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    jest: true
   },
   extends: [
     'plugin:react/recommended',
     'airbnb'
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -15,21 +16,35 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module'
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
   plugins: [
     'react',
-    'react-native'
+    '@typescript-eslint'
   ],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react-native/no-unused-styles': 2,
-    'react-native/split-platform-components': 2,
-    'react-native/no-inline-styles': 2,
-    'react-native/no-color-literals': 2,
-    'react-native/no-raw-text': 'off',
     'comma-dangle': [2, 'never'],
+    'no-underscore-dangle': 'off',
     'linebreak-style': 0,
     'global-require': 0,
-    'eslint linebreak-style': [0, 'error', 'windows']
-
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
   }
 };
