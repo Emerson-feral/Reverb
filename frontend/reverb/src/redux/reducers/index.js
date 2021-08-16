6290f65d25a5e6a5f8c1bc0a031e848729a912c0
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-16 18:12:23 +0200

    Changing files extension

--- frontend/reverb/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import guitarReducer from './guitarReducer';
-
-const rootReducer = combineReducers({
-  guitars: guitarReducer
-});
-export default rootReducer;
